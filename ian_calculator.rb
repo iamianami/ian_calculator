fec0c2bb496f66487f52649d91504272de74a6a6
iamianami <ian751029@gmail.com>
2015-01-01 18:04:29 +0800

    Do/While loop

--- ian_calculator.rb
+++ ian_calculator.rb
@@ -1,43 +1,60 @@
-def say(msg)
-  puts "------#{msg}------"
-end
+# def say(msg)
+#   puts "------#{msg}------"
+# end
 
 
 
-say("Please type your first number")
-number1 = gets.chomp.to_i
+# say("Please type your first number")
+# number1 = gets.chomp.to_i
 
-say("Please type your second number ")
-number2 = gets.chomp.to_i
+# say("Please type your second number ")
+# number2 = gets.chomp.to_i
 
-say("Choose 1)add 2)sub 3)multi 4) divided")
-operation = gets.chomp.to_i
+# say("Choose 1)add 2)sub 3)multi 4) divided")
+# operation = gets.chomp.to_i
 
 
-if operation == 1
-  result = number1 + number2
-elsif operation == 2
-  result = number1 - number2
-elsif operation == 3
-  result =  number1 * number2
-elsif operation == 4
-  result =  number1 / number2.to_f
-else
-  puts "Please type the number,not thing else"
-end
+# if operation == 1
+#   result = number1 + number2
+# elsif operation == 2
+#   result = number1 - number2
+# elsif operation == 3
+#   result =  number1 * number2
+# elsif operation == 4
+#   result =  number1 / number2.to_f
+# else
+#   puts "Please type the number,not thing else"
+# end
 
-puts "The total is #{result}"
+# puts "The total is #{result}"
 
-# begin                        
-#   puts number1 + number2
-# while operation == 1
-  
-#   puts number1 - number2
-# while operation == 2
  
-#   puts number1 * number2
-# while operation == 3
 
-#   puts number1 / number2.to_f
-# end while operation == 4
+  begin
+    puts "Enter your first number"
+    number1 = gets.chomp.to_i
+    
+    puts "Enter your second number"
+    number2 = gets.chomp.to_i
+    
+    puts "Choose 1)add,2)sub,3)multi,4)divided,please type the number"
+    operation = gets.chomp.to_i
 
+    if operation == 1
+      puts number1 + number2 
+    elsif operation == 2
+      puts number1 - number2
+    elsif operation == 3
+      puts number1 * number2
+    elsif operation == 4
+      puts number1 / number2.to_f
+    else
+      puts "Please choose a number to actice the operation"
+    end
+
+    puts "Do you want to count something again? (y/n)"
+   end while gets.chomp.downcase != "n"
+ 
+#Finally ,I figured it out...
+
+  
